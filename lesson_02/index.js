8d128a85f581352c8b8088ce7653c48e768fa6fd
i-webtest <stasbass80@gmail.com>
2022-12-07 18:55:32 +0300

    completed lesson_02

---
+++ lesson_02/index.js
@@ -0,0 +1,12 @@
+const goods = "стул";
+let quantityGoods = 5;
+const priceGoods = 3000;
+const category = "мебель";
+
+const totalSum = quantityGoods * priceGoods;
+
+console.log(`Наименование товара: ${goods}`);
+console.log(`Общая стоимость: ${totalSum} рублей`);
+
+const goodsOther = "ноутбук Macbook Pro";
+const categoryOther = "компьютеры";
