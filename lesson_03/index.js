4b065355e26af63735f904d148c2b92e3ff5b4ff
i-webtest <stasbass80@gmail.com>
2022-12-07 20:02:39 +0300

    fixed bug

--- lesson_03/index.js
+++ lesson_03/index.js
@@ -16,14 +16,14 @@
 }
 
 {
-  // const nameGoods = prompt('Наименование товара');
-  // const countGoods = +prompt('Количество товара');
-  // const categoryGoods = prompt('Категория товара');
-  // const priceGoods = +prompt('Цена товара');
-  // const totalPrice = countGoods * priceGoods;
-  // console.log(`Наименование товара:  ${typeof nameGoods}`);
-  // console.log(`Количество товара: ${typeof countGoods}`);
-  // console.log(`Категория товара: ${typeof categoryGoods}`);
-  // console.log(`Цена товара: ${typeof priceGoods}`);
-  // console.log(`На складе ${countGoods} единицы товара "${nameGoods}" на сумму ${totalPrice} рублей`);
+  const nameGoods = prompt('Наименование товара');
+  const countGoods = +prompt('Количество товара');
+  const categoryGoods = prompt('Категория товара');
+  const priceGoods = +prompt('Цена товара');
+  const totalPrice = countGoods * priceGoods;
+  console.log(`Наименование товара:  ${typeof nameGoods}`);
+  console.log(`Количество товара: ${typeof countGoods}`);
+  console.log(`Категория товара: ${typeof categoryGoods}`);
+  console.log(`Цена товара: ${typeof priceGoods}`);
+  console.log(`На складе ${countGoods} единицы товара "${nameGoods}" на сумму ${totalPrice} рублей`);
 }
