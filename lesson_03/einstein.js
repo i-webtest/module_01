e4e37865e12a9fe57f03a10dc0a62fb10ee9a087
i-webtest <stasbass80@gmail.com>
2022-12-07 19:59:04 +0300

    lesson_03

---
+++ lesson_03/einstein.js
@@ -0,0 +1,5 @@
+const myWeight = 74;
+const speedLight = 3e8;
+const total = myWeight * speedLight * speedLight;
+
+console.log(`Энергия моего тела: ${total}`);
---
+++ lesson_03/index.js
@@ -0,0 +1,29 @@
+{
+  const goods = 'стул';
+  let quantityGoods = 5;
+  const priceGoods = 3000;
+  const category = 'мебель';
+
+  const totalSum = quantityGoods * priceGoods;
+
+  // console.log(`Наименование товара: ${goods}`);
+  // console.log(`Общая стоимость: ${totalSum} рублей`);
+}
+
+{
+  const goodsOther = 'ноутбук Macbook Pro';
+  const categoryOther = 'компьютеры';
+}
+
+{
+  // const nameGoods = prompt('Наименование товара');
+  // const countGoods = +prompt('Количество товара');
+  // const categoryGoods = prompt('Категория товара');
+  // const priceGoods = +prompt('Цена товара');
+  // const totalPrice = countGoods * priceGoods;
+  // console.log(`Наименование товара:  ${typeof nameGoods}`);
+  // console.log(`Количество товара: ${typeof countGoods}`);
+  // console.log(`Категория товара: ${typeof categoryGoods}`);
+  // console.log(`Цена товара: ${typeof priceGoods}`);
+  // console.log(`На складе ${countGoods} единицы товара "${nameGoods}" на сумму ${totalPrice} рублей`);
+}
