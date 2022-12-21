b31137abd3f125899b43a184370bcfae270b8345
i-webtest <stasbass80@gmail.com>
2022-12-21 21:38:48 +0300

    fixed bugs in game01.js

--- lesson_12/game01.js
+++ lesson_12/game01.js
@@ -1,47 +1,4 @@
 'use strict';
-/*
-const getRandomNumber = () => Math.ceil(Math.random() * 100);
-
-const isNumber = (num) => !isNaN(parseFloat(num)) && isFinite(num);
-
-const startGame = () => {
-  const randomNumber = getRandomNumber();
-
-  const game = function () {
-    let num = prompt('Угадай число от 1 до 100');
-
-    if (num === null) {
-      alert('Игра окончена');
-      return;
-    }
-
-    if (isNumber(num)) {
-      const numUser = +num;
-
-      if (numUser > randomNumber) {
-        alert('Загаданное число меньше');
-        game();
-      } else if (numUser < randomNumber) {
-        alert('Загаданное число больше');
-        game();
-      } else if (numUser === randomNumber) {
-        confirm('Поздравляю, Вы угадали!!!');
-        startGame();
-      } else {
-        alert('Игра окончена');
-        return;
-      }
-    } else {
-      alert('Введи число!');
-      game();
-    }
-  };
-
-  game();
-};
-
-startGame();
-*/
 
 const getRandomNumber = () => Math.ceil(Math.random() * 100);
 
